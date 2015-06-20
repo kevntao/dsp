78af41697a47cece979739b8ed7c0046f34989d7
Kevin Chang <kevintao5@gmail.com>
2015-06-20 15:34:35 -0500

    updated markov files

--- python/markov.py
+++ python/markov.py
@@ -1,3 +1,34 @@
 #!/usr/bin/env python
 
-REPLACE THIS TEXT WITH YOUR PROGRAM
+import random
+import sys
+
+importfile = sys.argv[1]
+count = int(sys.argv[2])
+
+with open(importfile) as f:
+    input = f.read()
+
+dict_of_words = {}
+output = []
+list_of_words = input.split()
+list_of_words_count = len(list_of_words)
+
+for i in range(list_of_words_count-2):
+    key = (list_of_words[i], list_of_words[i+1])
+    if key in dict_of_words:
+        dict_of_words[key].append(list_of_words[i+2])
+    else:
+        dict_of_words[key] = [list_of_words[i+2]]
+
+rand = random.randint(0, list_of_words_count-3)
+w1, w2 = list_of_words[rand], list_of_words[rand+1]
+
+for i in range(count-1):
+    output.append(w1)
+    w1, w2 = w2, random.choice(dict_of_words[(w1, w2)])
+
+output.append(w2)
+print ' '.join(output)
+
+
